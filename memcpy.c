ffab581afea7661a67c8c47224d2af4e5e2984df
biboon <r.libaert@gmail.com>
2016-11-20 19:04:32 +0100

    Taking care of alignement now

--- memcpy.c
+++ memcpy.c
@@ -21,10 +21,13 @@ void *memcpy3(void *dst, const void *src, size_t size)
 {
 	char *_dst = dst;
 	const char *_src = src;
-	unsigned *_udst, *_usrc;
+	unsigned *_udst;
+	const unsigned *_usrc;
 
 	if (likely(size > 3)) {
-		switch (((unsigned long)_dst & 3)) {
+		unsigned buf_left, buf_right;
+		/* Align destination to word */
+		switch ((unsigned long)_dst & 3) {
 			case 1:
 				*_dst++ = *_src++;
 				--size;
@@ -35,25 +38,63 @@ void *memcpy3(void *dst, const void *src, size_t size)
 				*_dst++ = *_src++;
 				--size;
 		}
-		_udst = (unsigned *)_dst; _usrc = (unsigned *)_src;
-		while(size >= sizeof(unsigned)) {
-			*_udst++ = *_usrc++;
-			size -= sizeof(unsigned);
+		/* Choose a copy scheme based on the source alignement */
+		_udst = (void *)_dst;
+		switch((unsigned long)_src & 3) {
+			case 0:
+				_usrc = (const void *)_src;
+				for ( ; size >= sizeof(unsigned); size -= sizeof(unsigned)) {
+					*_udst++ = *_usrc++;
+				}
+				_src = (const void *)_usrc;
+				break;
+			case 1:
+				_usrc = (const void *)((unsigned long)_src & ~3);
+				buf_left = *_usrc++ >> 8;
+				for ( ; size >= sizeof(unsigned); size -= sizeof(unsigned)) {
+					buf_right = *_usrc;
+					*_udst++ = buf_left | (buf_right << 24);
+					buf_left = buf_right >> 8;
+				}
+				_src = (const void *)_usrc;
+				_src -= 3;
+				break;
+			case 2:
+				_usrc = (const void *)((unsigned long)_src & ~3);
+				buf_left = *_usrc++ >> 16;
+				for ( ; size >= sizeof(unsigned); size -= sizeof(unsigned)) {
+					buf_right = *_usrc;
+					*_udst++ = buf_left | (buf_right << 16);
+					buf_left = buf_right >> 16;
+				}
+				_src = (const void *)_usrc;
+				_src -= 2;
+				break;
+			case 3:
+				_usrc = (const void *)((unsigned long)_src & ~3);
+				buf_left = *_usrc++ >> 24;
+				for ( ; size >= sizeof(unsigned); size -= sizeof(unsigned)) {
+					buf_right = *_usrc;
+					*_udst++ = buf_left | (buf_right << 8);
+					buf_left = buf_right >> 24;
+				}
+				_src = (const void *)_usrc;
+				_src -= 1;
+				break;
 		}
-		_dst = (char *)_udst; _src = (const char *)_usrc;
+		_dst = (void *)_udst;
 	}
-	switch (((unsigned long)_dst & 3)) {
-		case 1:
+	switch (size) {
+		case 3:
 			*_dst++ = *_src++;
 			--size;
 		case 2:
 			*_dst++ = *_src++;
 			--size;
-		case 3:
+		case 1:
 			*_dst++ = *_src++;
 			--size;
 	}
-	while (size--) *(_dst++) = *(_src++);
 	return dst;
 }
 
@@ -74,11 +115,14 @@ void test(size_t size, struct task function)
 {
 	char _dst[size], _src[size];
 	char *dst = _dst, *src = _src;
-	unsigned loops = 0xFFFFFF / size, i;
+	unsigned loops = 0xFFFFFFFF / size, i;
+	//loops = 1;
 	clock_t start, stop;
 
+	memset(_src, -1, sizeof(_dst));
 	memset(_dst, 0, sizeof(_dst));
 	/* Start the loop */
+	src++; size--;
 	start = clock();
 	for (i = 0; i < loops; ++i)
 		function.function(dst, src, size);
@@ -104,13 +148,16 @@ int main(int argc, char const *argv[])
 {
 	unsigned i, j;
 	struct task functions[] = {
-		{ memcpy, "memcpy" },
-		{ memcpy2, "memcpy2" },
+//		{ memcpy, "memcpy" },
+//		{ memcpy2, "memcpy2" },
 		{ memcpy3, "memcpy3" }
 	};
 	size_t sizes[] = {
-		4 + 1, 64 + 1, 256 + 1,
-		4 * 1024, 64 * 1024, 256 * 1024
+		4 + 3, 64 + 3, 256 + 3,
+		4 * 1024 + 1, 64 * 1024 + 1, 256 * 1024 + 1,
+		4 * 1024 + 2, 64 * 1024 + 2, 256 * 1024 + 2,
+		4 * 1024 + 3, 64 * 1024 + 3, 256 * 1024 + 3,
+		1024 * 1024
 	};
 
 	for (i = 0; i < sizeof(functions) / sizeof(struct task); ++i) {
