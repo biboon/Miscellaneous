73a809ad9d1ba0576b515ea251907fb750020b3e
root <root@yuurei.localdomain>
2016-12-07 13:31:21 +0100

    New script to repair no BCD windows after updating Arch (Linux kernel ?)

--- Passthrough/qemu-windows-debug.sh
+++ Passthrough/qemu-windows-debug.sh
@@ -8,7 +8,7 @@ fi
 echo "Starting virtual machine"
 QEMU_HDD=/dev/disk/by-id/ata-SSD9SC120GEDA_PNY1210A00013166
 
-qemu-system-x86_64 -enable-kvm -name windows \
+qemu-system-x86_64 -enable-kvm -name windows -boot order=dc \
 -cpu host,kvm=off -smp sockets=1,cores=3,threads=2 -m 8G \
 -device vfio-pci,host=01:00.0,x-vga=on \
 -device vfio-pci,host=01:00.1 \
@@ -17,14 +17,13 @@ qemu-system-x86_64 -enable-kvm -name windows \
 -drive if=pflash,format=raw,readonly,file=/usr/share/ovmf/x64/ovmf_code_x64.bin \
 -drive if=pflash,format=raw,file=/usr/share/ovmf/x64/ovmf_vars_x64.bin \
 -drive file=$QEMU_HDD,format=raw,index=0,media=disk \
--boot order=c -vga none -nographic \
-\
--rtc base=localtime -k fr \
--nodefaults -serial none -parallel none \
--nodefconfig -no-user-config
-
+-drive file=/home/biboon/Documents/ISOs/en_windows_10_education_n_x64_dvd_6847236.iso,id=virtiocd,if=none \
+-device ide-cd,bus=ide.1,drive=virtiocd
 echo "Virtual machine stopped"
 exit 0
-
+# -vga none -nographic \
 # Use this to run the VM in the background
 # -daemonize -pidfile file
+# -rtc base=localtime -k fr \
+# -nodefaults -serial none -parallel none \
+# -nodefconfig -no-user-config -vnc :1
