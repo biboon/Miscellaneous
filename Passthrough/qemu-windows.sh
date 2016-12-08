d589991783b40a0897ce70eaf48aedf1ca8e9231
biboon <r.libaert@gmail.com>
2016-12-08 13:52:07 +0100

    Blbl

--- Passthrough/qemu-windows.sh
+++ Passthrough/qemu-windows.sh
@@ -6,23 +6,25 @@ then
 fi
 
 echo "Starting virtual machine"
+export QEMU_PA_SAMPLES=128
+export QEMU_AUDIO_DRV=pa
 QEMU_HDD=/dev/disk/by-id/ata-SSD9SC120GEDA_PNY1210A00013166
 
 qemu-system-x86_64 -enable-kvm -name windows \
 -cpu host,kvm=off -smp sockets=1,cores=3,threads=2 -m 8G \
--device vfio-pci,host=01:00.0,x-vga=on \
+-device vfio-pci,host=01:00.0 \
 -device vfio-pci,host=01:00.1 \
 -usbdevice host:2516:0011 \
 -usbdevice host:1e7d:2d50 \
 -drive if=pflash,format=raw,readonly,file=/usr/share/ovmf/x64/ovmf_code_x64.bin \
--drive if=pflash,format=raw,file=/usr/share/ovmf/x64/ovmf_vars_x64.bin \
 -drive file=$QEMU_HDD,format=raw,index=0,media=disk \
--netdev user,id=user.0 -device e1000,netdev=user.0 \
--boot order=c -vga none -nographic \
+-netdev user,id=vmnic -device e1000,netdev=vmnic \
+-vga none -nographic \
 \
--rtc base=localtime -k fr \
+-machine pc,accel=kvm,kernel_irqchip=on,mem-merge=off \
 -nodefaults -serial none -parallel none \
--nodefconfig -no-user-config
+-nodefconfig -no-user-config \
+-localtime -k fr
 
 echo "Virtual machine stopped"
 exit 0
