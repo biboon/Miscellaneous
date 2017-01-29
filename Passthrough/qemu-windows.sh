f6ba153782a04966e6e5b6bb842d25ab78f33b7c
biboon <r.libaert@gmail.com>
2017-01-29 23:52:16 +0100

    Adding freedesktop entry for starting VM in terminal, updated conky

--- Passthrough/qemu-windows.sh
+++ Passthrough/qemu-windows.sh
@@ -8,8 +8,8 @@ OVMF_VARS=/usr/share/ovmf/x64/ovmf_vars_x64.bin
 # Disable primary screen and set the second screen primary
 killall conky &> /dev/null
 xrandr \
---output HDMI1 --off \
---output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --primary
+--output HDMI-1 --off \
+--output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal --primary
 conky --quiet
 
 # Run the VM, taskset for CPU pinning
@@ -35,8 +35,8 @@ qemu-system-x86_64 -enable-kvm -name windows -boot order=c \
 # Restore first screen as primary and the second screen as secondary
 killall conky &> /dev/null
 xrandr \
---output HDMI1 --mode 2560x1440 --pos 0x0 --rotate normal --primary \
---output HDMI2 --off
+--output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --primary \
+--output HDMI-2 --off
 conky --quiet
 
 echo "Virtual machine stopped"
