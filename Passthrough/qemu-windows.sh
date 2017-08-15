6db18e7a8bc4e4d7b6e69841d10252bd3ea5a0b5
biboon <r.libaert@gmail.com>
2017-08-15 21:29:08 +0200

    update

--- Passthrough/qemu-windows.sh
+++ Passthrough/qemu-windows.sh
@@ -8,40 +8,45 @@ then
     [[ ! $REPLY =~ ^[Yy\n]$ ]] || exit 1
 fi
 
-echo "Starting virtual machine"
-export QEMU_AUDIO_DRV=alsa
-QEMU_HDD=/dev/disk/by-id/ata-SSD9SC120GEDA_PNY1210A00013166
-OVMF=/usr/share/OVMF/OVMF_CODE.fd
-OVMF_VARS=/usr/share/OVMF/OVMF_VARS.fd
 
 # Disable primary screen and set the second screen primary
 xrandr \
 --output HDMI-1 --off \
 --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal --primary
 
+
+echo "Starting virtual machine"
+export QEMU_AUDIO_DRV=alsa
+QEMU_HDD=/dev/disk/by-id/ata-SSD9SC120GEDA_PNY1210A00013166
+OVMF=/usr/share/ovmf/ovmf_code_x64.bin
+OVMF_VARS=/usr/share/ovmf/ovmf_vars_x64.bin
+
 # Run the VM, taskset for CPU pinning
 taskset EE \
 qemu-system-x86_64 -enable-kvm -name windows -boot order=c \
 -machine pc,accel=kvm,kernel_irqchip=on,mem-merge=off \
 -cpu host,kvm=off -smp sockets=1,cores=3,threads=2 -m 8G \
--drive if=virtio,format=raw,cache=none,aio=native,media=disk,file=$QEMU_HDD \
--device vfio-pci,host=01:00.0,multifunction=on \
--device vfio-pci,host=01:00.1 \
--netdev user,id=vmnic -device e1000,netdev=vmnic \
 -drive if=pflash,format=raw,readonly,file=$OVMF \
 -drive if=pflash,format=raw,file=$OVMF_VARS \
+-drive if=virtio,format=raw,cache=none,aio=native,media=disk,file=$QEMU_HDD \
+-netdev user,id=vmnic -device e1000,netdev=vmnic \
+-device vfio-pci,host=01:00.0,multifunction=on \
+-device vfio-pci,host=01:00.1 \
 -usbdevice host:2516:0011 \
 -usbdevice host:1038:1361 \
--vga none -nographic -soundhw ac97 \
+-usbdevice host:03f0:0317 \
 \
+-vga none -nographic -soundhw ac97 \
 -nodefaults -serial none -parallel none \
 -nodefconfig -no-user-config \
 -localtime -k fr
+
 
 # Restore first screen as primary and the second screen as secondary
 xrandr \
 --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --primary \
 --output HDMI-2 --off
+
 
 echo "Virtual machine stopped"
 exit 0
